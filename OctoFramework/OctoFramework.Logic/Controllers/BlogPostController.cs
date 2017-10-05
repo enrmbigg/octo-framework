d2f9b543f1e82d01931fc983c02ce4312bf7ef58
Michael Biggs <mikeylfc007@blueyonder.co.uk>
2017-10-05 19:00:59 +0100

    Removed Page View Test (due to not being viable)

--- OctoFramework/OctoFramework.Logic/Controllers/BlogPostController.cs
+++ OctoFramework/OctoFramework.Logic/Controllers/BlogPostController.cs
@@ -29,34 +29,5 @@ namespace OctoFramework.Logic.Controllers
             };
             return View("BlogPost", blogPostModel);
         }
-
-        public ActionResult RecordView(IPublishedContent model)
-        {
-            var logger = LoggerResolver.Current.Logger;
-            var dbContext = ApplicationContext.DatabaseContext;
-            var helper = new DatabaseSchemaHelper(dbContext.Database, logger, dbContext.SqlSyntax);
-            //Check if the DB table does NOT exist
-            if (helper.TableExist("PageViews"))
-            {
-                var sql = new Sql().Where<PageView>(x => x.Url == model.Url, dbContext.SqlSyntax);
-                var record = dbContext.Database.FirstOrDefault<PageView>(sql);
-                if (record == null)
-                {
-                    record = new PageView
-                    {
-                        Url = model.Url,
-                        PublishDate = model.CreateDate,
-                        Count = 0
-                    };
-                }
-                else
-                {
-                    record.Count = record.Count++;
-                }
-
-                dbContext.Database.Insert(record);
-            }
-            return new EmptyResult();
-        }
     }
 }
